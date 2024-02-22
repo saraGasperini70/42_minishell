b12ec676027190cdf4a646d42bba59de89cd297d
Sara Gasperini <sgasperi@e4r8p2.42roma.it>
2024-02-22 14:47:18 +0100

    Started ft_print_sortedenv

--- builtins/env/ft_print_sortedenv.c
+++ builtins/env/ft_print_sortedenv.c
@@ -6,10 +6,16 @@
 /*   By: sgasperi <sgasperi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 12:34:23 by sgasperi          #+#    #+#             */
-/*   Updated: 2024/02/20 12:34:24 by sgasperi         ###   ########.fr       */
+/*   Updated: 2024/02/22 14:45:04 by sgasperi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../builtins.h"
 
-int	ft_print_sortedenv(t_env *secret);
+void	ft_print_sortedenv(t_env *env)
+{
+	char	**env_matrix;
+	char	*env_str;
+
+	
+}
