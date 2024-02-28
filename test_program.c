fb087d24c1fdb4c2c6adf01361d027295c93a7c1
SaraG527 <119115726+SaraG527@users.noreply.github.com>
2024-02-28 13:54:00 +0100

    needs testing/debugging

--- test_program.c
+++ test_program.c
@@ -2,12 +2,15 @@
 
 void	initialize_mini(t_mini *mini)
 {
-	mini->env = NULL;
+	printf("Hello\n");
 	mini->exit = 0;
 	mini->in = 0;
 	mini->out = 0;
 	mini->ret = 0;
+	printf("Hello\n");
 	mini->secret_env = NULL;
+	mini->env = NULL;
+	printf("Hello\n");
 }
 
 int	main(void)
@@ -15,7 +18,10 @@ int	main(void)
 	t_mini *mini;
 
 	mini = NULL;
+	printf("Hello\n");
 	initialize_mini(mini);
+	printf("Hello\n");
 	ft_mini_exit(mini, "1");
+	printf("Hello\n");
 	return(0);
 }
