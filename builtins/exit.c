e976041d0bd634fb1b8a9b00b72a167005ccd019
SaraG527 <119115726+SaraG527@users.noreply.github.com>
2024-03-02 17:38:36 +0100

    modified exit

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: sgasperi <sgasperi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 12:13:18 by sgasperi          #+#    #+#             */
-/*   Updated: 2024/02/28 13:21:11 by sgasperi         ###   ########.fr       */
+/*   Updated: 2024/02/28 14:36:46 by sgasperi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,18 @@
 
 void	ft_mini_exit(t_mini *mini, char *cmd)
 {
-	printf("HEllo\n");
 	mini->exit = 1;
-	ft_putstr_fd("exit", STDERR);
-	if (cmd[1] == SUCCESS)
-		ft_putstr_fd("success", STDERR);
+	ft_putstr_fd("exit ", STDERR);
+	if (cmd[1] == 0)
+		ft_putstr_fd("success ", STDERR);
 	else
-		ft_putstr_fd("Error", STDERR);
+		ft_putstr_fd("Error ", STDERR);
 	if (cmd[1] && cmd[2])
-		ft_putstr_fd("Too many arguments", STDERR);
+		ft_putstr_fd("Too many arguments ", STDERR);
 	else if (ft_strisnum(&cmd[1]) == ERROR)
 	{
-		ft_putstr_fd("exit", STDERR);
-		ft_putstr_fd("numeric argument required", STDERR);
+		ft_putstr_fd("exit ", STDERR);
+		ft_putstr_fd("numeric argument required ", STDERR);
 	}
 	else if (cmd[1])
 		mini->ret = ft_atoi(&cmd[1]);
--- test_program.c
+++ test_program.c
@@ -2,26 +2,22 @@
 
 void	initialize_mini(t_mini *mini)
 {
-	printf("Hello\n");
 	mini->exit = 0;
 	mini->in = 0;
 	mini->out = 0;
 	mini->ret = 0;
-	printf("Hello\n");
 	mini->secret_env = NULL;
 	mini->env = NULL;
-	printf("Hello\n");
 }
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	t_mini *mini;
+	t_mini mini;
 
-	mini = NULL;
-	printf("Hello\n");
-	initialize_mini(mini);
-	printf("Hello\n");
-	ft_mini_exit(mini, "1");
-	printf("Hello\n");
-	return(0);
+	initialize_mini(&mini);
+	if (ac > 1)
+		if (ft_strncmp(av[1], "exit", ft_strlen(av[1])) == 0)
+			ft_mini_exit(&mini, av[2]);
+		else if (ft_strncmp(av[1], "exit", ft_strlen(av[1])) == 0)
+	return(SUCCESS);
 }
